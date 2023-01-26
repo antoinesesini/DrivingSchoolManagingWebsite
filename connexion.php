a6bc5aae8ffc63dc6afdda5a3c3a4b68f611ef77
sesinian <antoine.sesini@etu.utc.fr>
2023-01-26 16:29:00 +0100

    Initial commit

--- connexion.php
+++
@@ -1,8 +0,0 @@
-<?php
-$dbhost = 'tuxa.sme.utc';
-$dbuser = 'nf92a075';
-$dbpass = 'sKiIW0Xz';
-$dbname = 'nf92a075';
-$connect = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname) or die ('Error connecting to mysql');
-mysqli_set_charset($connect, 'utf8');
-?>
